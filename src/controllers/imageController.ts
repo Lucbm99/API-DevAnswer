3183bca33faae6ca8a544fbe5bc4f168d0819b18
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-09-06 21:31:43 -0300

    Fix - pegando imagens por id

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -3,6 +3,18 @@ import { Imagem } from './../models/Imagem';
 
 
 export const all = async (request: Request, response: Response) => {
-    const listaImagens = await Imagem.findAll();
-    response.json({ listaImagens })
+    const data = await Imagem.findAll();
+    response.json({ data })
+}
+
+export const getImagemById = async (request: Request, response: Response) => {
+    let { id } = request.params;
+    let imagem = await Imagem.findByPk(id);
+
+    if(imagem) {
+        response.json({ imagem })
+    } else {
+        response.json({ error: 'Imagem não encontrada'})
+    }
+
 }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,6 +6,7 @@ const router = Router();
 
 //pegando todas as imagens
 router.get('/imagens', ImagemController.all)
+router.get('/imagem/:id', ImagemController.getImagemById)
 
 //pegando imagem pelo id
 // router.get('/imagens', ImagemController.all)
